6d288e5ab2fae8c98894233e79f3a15f903eb72f
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2017-06-28 14:13:11 -0400

    updated files

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,9 +6,9 @@ $('a[href*="#"]')
   .click(function(event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
       && 
-      location.hostname == this.hostname
+      location.hostname === this.hostname
     ) {
       // Figure out element to scroll to
       var target = $(this.hash);
